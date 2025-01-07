b2e16fd71748d8b4ad0b8a1589e565eb9c4088e4
haroon-ahmedy92 <haroonamani92@gmail.com>
2025-01-07 13:23:22 +0300

    addition of assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,50 @@
 import './bootstrap';
 
+// Import  Scripts
+import './admin-calender-2e78789a';
+import './admin-customers-list-694fb00c';
+import './admin-dashboard-27890ed3';
+import './admin-product-add-b13b6e26';
+import './admin-shops-detail-25e7d401';
+import './admin-wallet-d7a556b6';
+
+// Import UI Scripts
+import './ui-advance-lightbox-91008499';
+import './ui-advance-rating-02c95d24';
+import './ui-advance-sweetalert-d4f62779';
+import './ui-advance-swiper-a611e9bc';
+import './ui-advance-tour-73f96522';
+
+// Import Other Scripts
+import './apexcharts.common-4fae8482';
+import './auth-23747e5d';
+import './colors-4fe812b9';
+import './countdown-fb292854';
+import './dropzone-f13a1568';
+import './free-mode-f21240fb';
+import './glightbox.min-8efe50e4';
+import './home-467f158b';
+import './index-a4e39586';
+import './navigation-a8a85143';
+import './nouislider-abb1ebb1';
+import './pages-gallery-c8f074cc';
+import './product-categories-5e2318ca';
+import './product-details-4bdfc89f';
+import './quill-1f5a4f93';
+import './sweetalert2@11';
+import './theme-9c065fc6';
+import './ui-chart-apex-dbbc49fe';
+import './ui-form-editor-a0635010';
+import './ui-form-picker-a2d4a7f9';
+import './ui-form-range-e38f041a';
+import './ui-form-select-a4d5ba6b';
+import './ui-form-upload-6973fd21';
+import './ui-form-validation-c08ec1f4';
+import './ui-maps-vector-ab96bff2';
+import './ui-table-datatable-16fb38fc';
+
+
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
